8f03787a385182c00db1b86fa1f1f986a947b58e
yinzhipeng123 <english126paopao@163.com>
2021-11-07 23:34:34 +0800

    commit_message

---
+++ clear_code.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+shopt -s extglob
+rm -rf !(mkdocs.yml|README.md|docs|site|.git|clear_code.sh|requirements.txt)
+pip freeze > requirements.txt
+mkdocs build
+mv site/* ./
+git add . -A 
+git commit -m "commit_message"
+git push
+
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+# MarkDown_Log
+
+```bash
+本文档由于MkDocs进行生成
+
+```
+
